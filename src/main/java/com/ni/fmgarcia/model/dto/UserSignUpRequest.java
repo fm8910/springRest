cf839da9f73a0269be73bb37eafe50734003af51
Felix Garcia <fm8910@gmail.com>
2023-08-07 16:12:42 -0600

    ajuste mensaje de validacion contrasena

--- src/main/java/com/ni/fmgarcia/model/dto/UserSignUpRequest.java
+++ src/main/java/com/ni/fmgarcia/model/dto/UserSignUpRequest.java
@@ -23,11 +23,12 @@ public class UserSignUpRequest {
     private String email;
 
     @NotBlank(message = "Favor ingresar una contraseña")
-    @Pattern(regexp="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$", message = "    Password must contain at least one digit [0-9].\n" +
-            "    Password must contain at least one lowercase Latin character [a-z].\n" +
-            "    Password must contain at least one uppercase Latin character [A-Z].\n" +
-            "    Password must contain at least one special character like ! @ # & ( ).\n" +
-            "    Password must contain a length of at least 8 characters and a maximum of 20 characters.")
+    @Pattern( regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$",
+            message = "Password must contain at least one digit [0-9].\n" +
+            "   La contraseña debe contener al menos un caracter en minusculas [a-z].\n" +
+            "   La contraseña debe contener al menos un caracter en mayusculas [A-Z].\n" +
+            "   La contraseña debe contener al menos un caracter especial ejemplo: ! @ # & ( ).\n" +
+            "   La contraseña debe tener un tamaño de al menos 8 caracteres y un maximo de 20 caracteres.")
     private String password;
 
     @NotNull(message = "Favor agregar al menos un telefono")
